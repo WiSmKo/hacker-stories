03e7230bdb7cf18011c0d066e2925b1c35dbac06
WiSmKo <william@thewhisk.dev>
2022-11-26 23:07:19 +0100

    add a function and basic array

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
-const title = 'React';
+function getTitle(title){
+  return title;
+}
+
+const array = [0,12,24,67]
 
 function App() {
 
   return (
     <div>
-      <h1>Hello {title}</h1>
+      <h1>
+        Hello {getTitle("Kitten")}, you have {array.at(2)} live remaining!  
+      </h1>
+
+      <label htmlFor='search'>Search: </label>
+      <input id='search' type='text' />
     </div>
   );
 }
